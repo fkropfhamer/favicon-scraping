c73ee46d7c9bf40467f3f58a9c9e33bee8581cbd
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-08-06 14:29:34 +0200

    cleanup

--- scrape.py
+++ scrape.py
@@ -1,25 +1,29 @@
 import requests
 import csv
 
+def main():
 
-with open('top-1m.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    with open('top-1m.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
 
-    for row in csv_reader:
-        if (row[0] == '20'):
-            break 
-        
-        try:
+        for row in csv_reader:
+            if (row[0] == '20'):
+                break 
+            
+            try:
 
-            r = requests.get(f"https://www.{row[1]}/favicon.ico", allow_redirects=True)
-            content_type = r.headers.get('content-type')
+                r = requests.get(f"https://www.{row[1]}/favicon.ico", allow_redirects=True)
+                content_type = r.headers.get('content-type')
 
-            # print(r.headers)
+                # print(r.headers)
 
-            if (r.status_code == 200):
-                with open(f"./files/{row[1]}.ico", 'wb') as file:
-                    file.write(r.content)
-        
-        except:
-            print('something went wrong!')
+                if (r.status_code == 200):
+                    with open(f"./files/{row[1]}.ico", 'wb') as file:
+                        file.write(r.content)
+            
+            except:
+                print('something went wrong!')
 
+if __name__ == "__main__":
+    main()
+    
