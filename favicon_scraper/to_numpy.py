5f4c4a54246760b48ec104e14a8184d96be96fb9
fkropfhamer <fabian.kropfhamer@gmail.com>
2020-10-14 23:36:35 +0200

    added error handling on to numpy conversion

--- favicon_scraper/to_numpy.py
+++ favicon_scraper/to_numpy.py
@@ -11,22 +11,24 @@ def save_to_numpy():
     numpy_arrays = None
 
     for filename in os.listdir(base_path):
-        with Image.open(f'{base_path}/{filename}') as im:
-            resized = im.resize((32, 32))
-            resizedRGB = resized.convert(mode='RGB')
+        try:
+            with Image.open(f'{base_path}/{filename}') as im:
+                resized = im.resize((32, 32))
+                resizedRGB = resized.convert(mode='RGB')
 
-            imageArray = np.array(resizedRGB)
-            imageArray = imageArray.reshape((1, 32, 32, 3))
+                imageArray = np.array(resizedRGB)
+                imageArray = imageArray.reshape((1, 32, 32, 3))
 
-            if imageArray.shape != (1, 32, 32, 3):
-                print('wrong shape')
+                if imageArray.shape != (1, 32, 32, 3):
+                    print('wrong shape')
 
-            if numpy_arrays is None:
-                numpy_arrays = imageArray
-                continue
+                if numpy_arrays is None:
+                    numpy_arrays = imageArray
+                    continue
             
-            numpy_arrays = np.append(numpy_arrays, imageArray, axis=0)
-
+                numpy_arrays = np.append(numpy_arrays, imageArray, axis=0)
+        except:
+            print('something went wrong')
 
     print(numpy_arrays.shape)
 
